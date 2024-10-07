f71156762fe6d810a370fe2bf35fca64de2c31b6
sayandeepbanerjee1 <sayandeepbanerjee224@gmail.com>
2024-10-08 02:31:18 +0530

    first

--- src/App.jsx
+++ src/App.jsx
@@ -11,9 +11,9 @@ function App() {
       <div>
         <Navbar />
         <Routes>
-          <Route exact path ="/" element = {<Home />} />
-          <Route path ="/about" element = {<About />} />
-          <Route path ="/services" element = {<ServicesSection/>} />
+          <Route exact path ="./" element = {<Home />} />
+          <Route path ="./about" element = {<About />} />
+          <Route path ="./services" element = {<ServicesSection/>} />
         </Routes>
       </div>
     </Router>
